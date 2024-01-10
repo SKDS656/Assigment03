254fead77a5c4d759fedeabf6fc755ffb966d2a1
SKDS656 <146479432+SKDS656@users.noreply.github.com>
2024-01-10 08:45:40 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export const PORT = 3000;
+
+export const mongoDBURL =  'mongodb+srv://shehzad:shehzad123@cluster2.2kvlk5m.mongodb.net/';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
